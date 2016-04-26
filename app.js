d42c0ed8fe2dd79573668a93665bca6ba6a8d281
David Pryce <dpryce@seek.com.au>
2016-04-27 08:24:37 +1000

    get only middleware working - error returned.
    Error returned for incorrect method.

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var app = express();
 
+// use getonly middleware
+var getonly = require('./getonly');
+app.use(getonly);
 
-
-/* serve all files in public folder */
+//serve all files in public folder
 app.use(express.static('public'));
 
 
-/* get request for cities */
+// get request for cities
 app.get('/cities', function(request, response){
   var cities = ['Melbourne', 'Sydney', 'Brisbane','Adelaide','Perth','Darwin','Hobart'];
   response.json(cities);
 });
 
 
-/* local host */
+// local host
 app.listen(3001, function() {
     console.log("Running Express");
 });
---
+++ getonly.js
@@ -0,0 +1,10 @@
+/* Create middle ware to only accept get requests */
+module.exports = function(request, response, next){
+  if (request.method === 'GET') {
+    next();
+    } else {
+      //response.send('Method is not allowed');
+      // json response
+      response.status(405).json('405 - Method is not allowed');
+    }
+};
