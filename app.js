30a3d527127e15399fc6b4d8ed3b049e829b513d
David Pryce <dpryce@seek.com.au>
2016-04-27 09:03:41 +1000

    filter by name working

--- app.js
+++ app.js
@@ -8,12 +8,40 @@ app.use(getonly);
 //serve all files in public folder
 app.use(express.static('public'));
 
+// cities arrary
+var cities = ['Melbourne',
+'Sydney',
+'Brisbane',
+'Adelaide',
+'Perth',
+'Darwin',
+'Hobart',
+'Canberra'];
+
 
 // get request for cities
 app.get('/cities', function(request, response){
-  var cities = ['Melbourne', 'Sydney', 'Brisbane','Adelaide','Perth','Darwin','Hobart'];
-  response.json(cities);
+  if(request.query.search){
+    response.json(citySearch(request.query.search));
+  }
+  else {
+    response.json(cities);
+  }
 });
+
+
+
+
+// create function to filter by city name
+function citySearch (keyword) {
+  // i case sensitive matching
+  var regexp = RegExp(keyword, 'i');
+  var result = cities.filter(function (city){
+    return city.match(regexp);
+  });
+
+  return result;
+}
 
 
 // local host
